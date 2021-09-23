ca331fa214a861a361bb4d0706618279c29179f4
Wessel Fick <wesselfick@geotab.com>
2021-09-22 20:57:50 -0400

    Fix update

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+import '!style-loader!css-loader!sass-loader!./../projects/mya-controls/src/styles/global.scss';
+
+setCompodocJson(docJson);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+}
